141fd3fd20cf04a35f25204f243e9774fa5f8918
Brijesh Bittu <brijesh42@gmail.com>
2025-02-28 16:53:48 +0530

    Some documentation and refactor

--- packages/engine-babel-plugin/src/generateCss.ts
+++ packages/engine-babel-plugin/src/generateCss.ts
@@ -7,8 +7,61 @@ function generateRandomNumberBetween(min: number = 1, max: number = 1000) {
 /**
  * Transforms the given CSS string to browser understanable CSS.
  * Handles `@breakpoints` custom at-rule as well as custom media queries like `@media (--xs)` etc.
+ *
+ * @param cssStr The css string to be transformed.
+ * @param filename The filename to be used in sourcemap if enabled.
+ * @param supportsRuntime If false, then the custom transformations won't be applied.
+ *
+ * For the input -
+ *
+ * @example
+ *
+ * ```css
+.mui-Button {
+  color: red;
+  @breakpoints {
+    &.size-1 {
+      padding: var(--size-1);
+    }
+  }
+
+  @media (--xs) {
+    &:disabled {
+      color: grey;
+    }
+  }
+}
+ * ```
+ *
+ * will output
+ *
+ * ```css
+ *
+ *  .mui-Button {
+      color: red;
+    }
+    #__separator-start-0745 {
+      height: 17px;
+    }
+    .mui-Button.size-1.__breakpoint_placeholder__ {
+      padding: var(--size-1);
+    }
+    #__separator-end-989 {
+      height: 17px;
+    }
+    @media (--__breakpoint_query_placeholder__xs) {
+      .mui-Button:disabled {
+        color: gray;
+      }
+    }```
+ * Then through string manipulation, a new AST is generated that replaces the `#__separator-start`. `#__separator-end` and
+    `__breakpoint_query_placeholder__` values with runtime values. 
  */
-export function generateCss(cssStr: string, filename: string = '') {
+export function generateCss(
+  cssStr: string,
+  filename: string = '',
+  supportsRuntime: boolean = true
+) {
   const result = transform({
     minify: true,
     filename,
@@ -22,40 +75,46 @@ export function generateCss(cssStr: string, filename: string = '') {
     visitor: {
       Rule: {
         media(rule) {
-          const { value } = rule;
-          // handles @media (--xs) {}
-          value.query.mediaQueries = value.query.mediaQueries.map((q) => {
-            if (
-              q.condition?.type === 'feature' &&
-              q.condition.value.type === 'boolean' &&
-              q.condition.value.name.startsWith('--')
-            ) {
-              q.condition.value.name = `--__breakpoint_query_placeholder__${q.condition.value.name.substring(2)}`;
-            }
-            return q;
-          });
-          // handles @media (--xs) and (--lg) {}
-          value.query.mediaQueries = value.query.mediaQueries.map((q) => {
-            if (q.condition?.type === 'operation') {
-              q.condition.conditions = q.condition.conditions.map((c) => {
-                if (
-                  c.type === 'feature' &&
-                  c.value.type === 'boolean' &&
-                  c.value.name.startsWith('--')
-                ) {
-                  c.value.name = `--__breakpoint_query_placeholder__${c.value.name.substring(2)}`;
-                }
-                return c;
-              });
-            }
-            return q;
-          });
+          if (supportsRuntime) {
+            const { value } = rule;
+            // handles @media (--xs) {}
+            value.query.mediaQueries = value.query.mediaQueries.map((q) => {
+              if (
+                q.condition?.type === 'feature' &&
+                q.condition.value.type === 'boolean' &&
+                q.condition.value.name.startsWith('--')
+              ) {
+                q.condition.value.name = `--__breakpoint_query_placeholder__${q.condition.value.name.substring(2)}`;
+              }
+              return q;
+            });
+            // handles @media (--xs) and (--lg) {}
+            value.query.mediaQueries = value.query.mediaQueries.map((q) => {
+              if (q.condition?.type === 'operation') {
+                q.condition.conditions = q.condition.conditions.map((c) => {
+                  if (
+                    c.type === 'feature' &&
+                    c.value.type === 'boolean' &&
+                    c.value.name.startsWith('--')
+                  ) {
+                    c.value.name = `--__breakpoint_query_placeholder__${c.value.name.substring(2)}`;
+                  }
+                  return c;
+                });
+              }
+              return q;
+            });
+          }
+
           return rule;
         },
         custom: {
           breakpoints(rule) {
             const result = [...rule.body.value];
 
+            if (!supportsRuntime) {
+              return result;
+            }
             result.forEach((style) => {
               if (style.type !== 'style') {
                 return;
@@ -120,6 +179,7 @@ export function generateCss(cssStr: string, filename: string = '') {
                 },
               },
             });
+
             return result;
           },
         },
